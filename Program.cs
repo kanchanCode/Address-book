5a7e548c2b30c6199db2255078f4470aff8e5c0b
Kanchan Jaiswal <91010576+kanchanCode@users.noreply.github.com>
2021-10-30 10:28:39 +0530

    UC1-UC4

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            int command=0;
+            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("1. To add new Details of Person");
+            Console.WriteLine("2. To delete Details of Person using name");
+            Console.WriteLine("3. To list the Details of Person");
+            Console.WriteLine("4. To edit Details of Person using name");
+            Console.WriteLine("5. To exit");
+           
+            while (command != 5)
+            {
+                
+                Console.WriteLine("Enter your choice");
+                command = Convert.ToInt32(Console.ReadLine());
+                if (command == 1 || command == 2 || command == 3 || command == 4)
+                {
+                    switch (command)
+                    {
+                        case 1:
+
+                            Console.Clear();
+                            ContactDetails.AddPerson();
+                            Console.WriteLine("Press 1 to add, 2 to remove , 3 to list all , 4 to edit and 5 to exit ");
+                            break;
+                        case 2:
+                            Console.Clear();
+                            ContactDetails.DeletePerson();
+                            Console.WriteLine("Press 1 to add, 2 to remove , 3 to list all , 4 to edit and 5 to exit ");
+                            break;
+                        case 3:
+                            Console.Clear();
+                            ContactDetails.ListPeople();
+                            Console.WriteLine("Press 1 to add, 2 to remove , 3 to list all , 4 to edit and 5 to exit ");
+                            break;
+                        case 4:
+                            Console.Clear();
+                            ContactDetails.Editperson();
+                            Console.WriteLine("Press 1 to add, 2 to remove , 3 to list all , 4 to edit and 5 to exit ");
+                            break;
+                        default:
+                            Console.WriteLine("invalid choice");
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid choice");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ UC1ContactDetails.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AddressBook
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string mobileNo { get; set; }
+        public string address { get; set; }
+        public string state { get; set; }
+
+    }
+    public class ContactDetails
+    {
+        public static LinkedList<Person> contactList = new LinkedList<Person>();
+
+
+        public static void AddPerson()
+        {
+            Person person = new Person();
+
+            Console.Write("Enter name :");
+            person.name = Console.ReadLine();
+
+            Console.Write("Enter mobile number :");
+            person.mobileNo = Console.ReadLine();
+
+            Console.Write("Enter Address :");
+            person.address = Console.ReadLine();
+
+            Console.Write("Enter state :");
+            person.state = Console.ReadLine();
+
+            contactList.AddLast(person);
+
+        }
+
+        public static void PrintPerson(Person person)
+        {
+            Console.WriteLine("Name: " + person.name);
+
+            Console.WriteLine("Mobile Number: " + person.mobileNo);
+            Console.WriteLine("Address : " + person.address);
+
+            Console.WriteLine("-------------------------------------------");
+
+        }
+
+        public static void ListPeople()
+        {
+            if (contactList.Count == 0)
+            {
+                Console.WriteLine("Your address book is empty. Press any key to continue.");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Here are the current people in your address book:\n");
+            foreach (var person in contactList)
+            {
+                PrintPerson(person);
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+        }
+
+        public static void DeletePerson()
+        {
+            Console.WriteLine("Enter the name of the person you would like to remove.");
+            string name = Console.ReadLine();
+            Person person = contactList.FirstOrDefault(x => x.name.ToLower() == name.ToLower());
+            if (person == null)
+            {
+                Console.WriteLine("That person could not be found. Press any key to continue");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Are you sure you want to remove this person from your address book? (Y/N)");
+            PrintPerson(person);
+
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                contactList.Remove(person);
+                Console.WriteLine("Person removed. Press any key to continue.");
+                Console.ReadKey();
+            }
+
+        }
+
+        public static void Editperson()
+        {
+            Console.WriteLine("Enter the name of the person you would like to edit.");
+            string name = Console.ReadLine();
+            Person person = contactList.FirstOrDefault(x => x.name.ToLower() == name.ToLower());
+            if (person == null)
+            {
+                Console.WriteLine("That person could not be found. Press any key to continue");
+                Console.ReadKey();
+                return;
+            }
+
+
+            Console.WriteLine("Are you sure you want to edit this person from your address book? (Y/N)");
+            PrintPerson(person);
+
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                Console.WriteLine("enter new details");
+                contactList.Remove(person);
+                AddPerson();
+            }
+
+        }
+    }
+}
+
+
+
